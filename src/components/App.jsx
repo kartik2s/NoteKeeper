e1f696d97c9d7f84963b0d705b96bb8a582fc8bb
Kartik Sharma <ks884024@gmail.com>
2026-06-02 10:31:42 +0530

    Update Form portion to add data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React ,{ useState }from "react";
 import Header from "./Header.jsx";
 import Note from "./Note.jsx";
 import Footer from "./Footer.jsx";
-import notes from "../notes.js";
+import CreateArea from "./CreateArea.jsx";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newnote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newnote];
+    })
+  }
+
+  function deleteNote(id){
+    setNotes(prevNotes =>{
+      return prevNotes.filter((noteItem, index)=>{
+        return index !== id;
+      })
+    })
+  }
+
   return (
     <div>
       <Header />
-      {notes.map(noteItem => {
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
         return (
-          <Note key={noteItem.key}
-            title={noteItem.title}
-            content={noteItem.content}
-          />
-        )
+      <Note
+        key={index}
+        id={index}
+        title={noteItem.title}
+        content={noteItem.content}
+        onDelete = {deleteNote}
+      />
+      )
       })}
       <Footer />
     </div>
